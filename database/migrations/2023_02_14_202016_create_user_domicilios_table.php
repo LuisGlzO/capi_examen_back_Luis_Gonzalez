25b0d4992167fa2eab1934ea1cf09c3517b119da
unknown <luisvip16@gmail.com>
2023-02-14 14:36:18 -0600

    Migracion tabla user_domicilio creada

---
+++ database/migrations/2023_02_14_202016_create_user_domicilios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDomiciliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilios', function (Blueprint $table) {
+            $table->id();
+            $table->string('domicilio');
+            $table->string('numero_exterior'); //Considerando que puede ser por ejemplo 34A y 34B
+            $table->string('colonia');
+            $table->string('cp');
+            $table->string('ciudad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilios');
+    }
+}
